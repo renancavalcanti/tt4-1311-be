4bf39703d4c27b820a8371b66549ea14d1e35473
Renan Cavalcanti <renan_cavalcanti@hotmail.com>
2026-03-17 11:18:05 -0400

    chore: register controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,49 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 
+const register = async (req, res) => {
+    try{
+        const { name, email, password } = req.body;
+        
+        if(!name || !email || !password){
+            return res.status(400).json({
+                message: "Name, Email and Password are required!"
+            });
+        }
 
-const register = (req, res) => {
-    console.log(req);
-    return res.status(201).json({message: "User created!"});
+        const existingUser =  await User.findOne({email: String(email).toLowerCase()});
+
+        if(existingUser){
+            return res.status(409).json({message: "Email is already registered!"});
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        return res.status(201).json({
+            message: "User created!",
+            data: {
+                user: {
+                    id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    createdAt: user.createdAt,
+                    updatedAt: user.updatedAt
+                }
+            }
+        });
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({message: "Error while regestering user!"});
+    }
+    
 }
 
 module.exports = { register };
